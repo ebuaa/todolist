846a823ba7890ebbac8809423493d3f23a453d5d
ebuaa <axel.aube@viacesi.fr>
2024-04-02 10:37:04 +0200

    create task.cs

---
+++ todolist/task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todolist
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
